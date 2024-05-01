fe837d907812501d24ba78a9af006cf3ecfe2af8
정석민 <jyeong1212@naver.com>
2024-05-01 21:12:12 +0900

    컴포넌트로 다 분리

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+function Header({ children }) {
+  return <h1>{children}</h1>;
+}
+
+function Article({ content, title }) {
+  return (
+    <article>
+      <h2>{title}</h2>
+      <p>{content}</p>
+    </article>
+  );
+}
+
+function Nav({ list }) {
+  return (
+    <nav>
+      <ol>
+        {list.map((item, id) => (
+          <li key={id}>
+            <div>{item.title}</div>
+          </li>
+        ))}
+      </ol>
+    </nav>
+  );
+}
 
 function App() {
+  const list = [
+    { title: "이름", desc: "정석민" },
+    { title: "생년월일", desc: "2000.03.27" },
+    { title: "학력", desc: "한국항공대학교" },
+  ];
+  const title = "인사";
+  const content = "안녕하세요. 잘 부탁드립니다.";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header>정석민 이력서</Header>
+      <Nav list={list}></Nav>
+      <Article title={title} content={content}></Article>
+    </>
   );
 }
 
