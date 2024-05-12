4652e3edbf9efd142fdf2c299b0469a1d6649bdc
정석민 <jyeong1212@naver.com>
2024-05-12 15:25:01 +0900

    fix: state version 1

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ function Article({ content, title }) {
   );
 }
 
-function Nav({ list }) {
+function Nav({ onChangeMode, list }) {
   return (
     <nav>
       <ol>
         {list.map((item) => (
-          <li key={item.id}>
+          <li key={item.id} onChangeMode={onChangeMode}>
             <div>{item.title}</div>
           </li>
         ))}
@@ -28,19 +28,29 @@ function Nav({ list }) {
 }
 
 function App() {
+  let mode = "HOME";
+
   const list = [
     { id: 1, title: "이름", desc: "정석민" },
     { id: 2, title: "생년월일", desc: "2000.03.27" },
     { id: 3, title: "학력", desc: "한국항공대학교" },
   ];
 
-  const title = "인사";
-  const content = "안녕하세요. 정석민입니다.";
+  let title;
+  let content;
+
+  if (mode === "HOME") {
+    title = "인사";
+    content = "안녕하세요. 정석민입니다.";
+  } else if (mode === "READ") {
+    title = "이력서 문항";
+    content = "이력서 내용";
+  }
 
   return (
     <>
       <Header onChangeMode={() => alert("hi")}>정석민 이력서</Header>
-      <Nav list={list}></Nav>
+      <Nav list={list} onChangeMode={() => (mode = "READ")}></Nav>
       <Article title={title} content={content}></Article>
     </>
   );
