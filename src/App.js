ab11aeafa11e06f65f3b8ebd43ed13ebcd42b1fb
정석민 <jyeong1212@naver.com>
2024-05-12 17:08:39 +0900

    fix: state version 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import "./App.css";
 
 function Header({ onChangeMode, children }) {
-  return <h1 onClick={onChangeMode}>{children}</h1>;
+  return <h1 onClick={() => onChangeMode()}>{children}</h1>;
 }
 
 function Article({ content, title }) {
@@ -18,7 +19,7 @@ function Nav({ onChangeMode, list }) {
     <nav>
       <ol>
         {list.map((item) => (
-          <li key={item.id} onChangeMode={onChangeMode}>
+          <li key={item.id} onClick={() => onChangeMode(item.id)}>
             <div>{item.title}</div>
           </li>
         ))}
@@ -28,12 +29,13 @@ function Nav({ onChangeMode, list }) {
 }
 
 function App() {
-  let mode = "HOME";
+  const [mode, setMode] = useState("HOME");
+  const [id, setId] = useState(-1);
 
   const list = [
-    { id: 1, title: "이름", desc: "정석민" },
-    { id: 2, title: "생년월일", desc: "2000.03.27" },
-    { id: 3, title: "학력", desc: "한국항공대학교" },
+    { id: 0, title: "이름", desc: "정석민" },
+    { id: 1, title: "생년월일", desc: "2000.03.27" },
+    { id: 2, title: "학력", desc: "고려대학교" },
   ];
 
   let title;
@@ -43,14 +45,20 @@ function App() {
     title = "인사";
     content = "안녕하세요. 정석민입니다.";
   } else if (mode === "READ") {
-    title = "이력서 문항";
-    content = "이력서 내용";
+    title = list[id].title;
+    content = list[id].desc;
   }
 
   return (
     <>
-      <Header onChangeMode={() => alert("hi")}>정석민 이력서</Header>
-      <Nav list={list} onChangeMode={() => (mode = "READ")}></Nav>
+      <Header onChangeMode={() => setMode("HOME")}>정석민 이력서</Header>
+      <Nav
+        list={list}
+        onChangeMode={(_id) => {
+          setMode("READ");
+          setId(_id);
+        }}
+      ></Nav>
       <Article title={title} content={content}></Article>
     </>
   );
