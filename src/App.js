cbafe41200138bea87bf64a2a18f135382208176
정석민 <jyeong1212@naver.com>
2024-05-12 14:43:46 +0900

    fix: Props 단원 저장

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 function Header({ children }) {
@@ -17,8 +18,8 @@ function Nav({ list }) {
   return (
     <nav>
       <ol>
-        {list.map((item, id) => (
-          <li key={id}>
+        {list.map((item) => (
+          <li key={item.id}>
             <div>{item.title}</div>
           </li>
         ))}
@@ -29,12 +30,14 @@ function Nav({ list }) {
 
 function App() {
   const list = [
-    { title: "이름", desc: "정석민" },
-    { title: "생년월일", desc: "2000.03.27" },
-    { title: "학력", desc: "한국항공대학교" },
+    { id: 1, title: "이름", desc: "정석민" },
+    { id: 2, title: "생년월일", desc: "2000.03.27" },
+    { id: 3, title: "학력", desc: "한국항공대학교" },
   ];
+
   const title = "인사";
-  const content = "안녕하세요. 잘 부탁드립니다.";
+  const content = "안녕하세요. 정석민입니다.";
+
   return (
     <>
       <Header>정석민 이력서</Header>
